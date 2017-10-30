eefb35dbbd6459e1a8fdb24c5f062abc383b4fc7
Khris <khristiandlw@gmail.com>
2017-10-30 10:16:40 -0300

    Add project files.

---
+++ XMLDemo/ReadingXML.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReadingXML.aspx.cs" Inherits="XMLDemo.ReadingXML" ValidateRequest="false"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #TextArea1 {
+            height: 181px;
+            width: 562px;
+        }
+        #txtDisplay {
+            height: 216px;
+            width: 618px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="BtnXPath" runat="server" Text="XPath Single Node" OnClick="BtnXPath_Click" />
+            <asp:Button ID="BtnXPathSearch" runat="server" Text="XPath Recursion Seach" OnClick="Button2_Click"/>
+            <asp:Button ID="BtnXPathDocument" runat="server" Text="Button" OnClick="Button3_Click"/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <textarea id="txtDisplay" runat="server" cols="30" rows="50"></textarea>       
+        </div>
+    </form>
+</body>
+</html>
---
+++ XMLDemo/ReadingXML.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace XMLDemo
+{
+    public partial class ReadingXML : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnXPath_Click(object sender, EventArgs e)
+        {
+            txtDisplay.InnerText = "";
+            XmlDocument document = new XmlDocument();
+            document.Load(Server.MapPath(("~/Menu.xml")));
+            XmlNode food = document.SelectSingleNode("breakfast_menu/food[name = 'Belgian Waffles']");
+            txtDisplay.InnerText = food.InnerXml;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            txtDisplay.InnerText = "";
+            XmlDocument document = new XmlDocument();
+            document.Load(Server.MapPath(("~/Menu.xml")));
+            XmlNodeList foods = document.SelectNodes("//name");
+            foreach (XmlNode food in foods)
+            {
+                txtDisplay.InnerText += food.InnerXml + Environment.NewLine;
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
